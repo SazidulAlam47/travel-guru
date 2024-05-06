f572aeaf18aa2ff1fcd3709f18d8a81e056b4880
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-06 20:37:39 +0600

    hotels page

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -6,28 +6,30 @@ const Hotels = () => {
     const { tag } = useParams();
     const place = allPlaces.find((place) => place.tag === tag);
 
-    console.log(place.map);
-
     return (
-        <div className="container mx-auto px-3 md:px-6">
-            <div className="border-t-2 grid grid-cols-1 md:grid-cols-2 gap-8 pt-4">
-                <div>
-                    <h3 className="font-bold text-2xl">Stay in {place.name}</h3>
-                    {place.hotels.map((hotel, idx) => (
-                        <SingleHotel key={idx} hotel={hotel} />
-                    ))}
-                </div>
-                <div>
-                    <iframe
-                        src={place.map}
-                        width="100%"
-                        height="100%"
-                        className="border-0 rounded-xl"
-                        allowFullScreen=""
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                        title="Google Maps"
-                    ></iframe>
+        <div className="container mx-auto px-3 md:px-6 pb-6">
+            <div className="border-t-2">
+                <h3 className="font-bold text-2xl pt-3">
+                    Stay in {place.name}
+                </h3>
+                <div className="flex flex-col md:flex-row gap-4 pt-1">
+                    <div className="w-[55%]">
+                        <div className="space-y-4 pt-2">
+                            {place.hotels.map((hotel, idx) => (
+                                <SingleHotel key={idx} hotel={hotel} />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="w-[45%]">
+                        <iframe
+                            src={place.map}
+                            width="100%"
+                            height="100%"
+                            className="border-0 rounded-xl"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Google Maps"
+                        ></iframe>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Hotels/SingleHotel.jsx
+++ src/pages/Hotels/SingleHotel.jsx
@@ -3,23 +3,42 @@ import { FaStar } from "react-icons/fa6";
 
 const SingleHotel = ({ hotel }) => {
     return (
-        <div>
-            <div>
-                <img src={hotel.img} alt={hotel.name} />
+        <div className="flex flex-col md:flex-row gap-3">
+            <div className="w-[45%]">
+                <img
+                    src={hotel.img}
+                    alt={hotel.name}
+                    className="rounded-lg h-full"
+                />
             </div>
-            <div>
-                <h4 className="font-bold">{hotel.name}</h4>
-                <p>
-                    {hotel.guests} guests {hotel.bedrooms} bedrooms {hotel.beds}{" "}
-                    beds {hotel.baths} baths
-                </p>
-                <p>{hotel.description}</p>
-                <p>Cancellation flexibility {hotel.cancellationFlexibility}</p>
-                <div>
-                    <div>
-                        <FaStar /> {hotel.rating}
+            <div className="w-[55%]">
+                <h4 className="font-semibold text-xl">{hotel.name}</h4>
+                <div className="text-[#6A6A6A] space-y-1">
+                    <p className="space-x-3">
+                        <span>{hotel.guests} guests</span>
+                        <span>{hotel.bedrooms} bedrooms</span>
+                        <span>{hotel.beds} beds</span>
+                        <span>{hotel.baths} baths</span>
+                    </p>
+                    <p>{hotel.description}</p>
+                    <p>
+                        Cancellation flexibility {hotel.cancellationFlexibility}
+                    </p>
+                </div>
+                <div className="flex justify-between items-center pt-1">
+                    <div className="flex items-center gap-1">
+                        <span className="text-primary">
+                            <FaStar size={20} />
+                        </span>{" "}
+                        {hotel.rating}
                     </div>
-                    <div>${hotel.rent}/night $00 total</div>
+                    <div>
+                        <span className="font-semibold">${hotel.rent}</span>
+                        /night{" "}
+                        <span className="text-[#898989] text-sm pl-1">
+                            $00 total
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
