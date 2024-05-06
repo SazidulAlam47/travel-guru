96e6f4dcd4d29e1c1391cb14be8f9de2918f0ebb
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-06 20:49:16 +0600

    cox map fixed

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -21,14 +21,27 @@ const Hotels = () => {
                         </div>
                     </div>
                     <div className="w-[45%]">
-                        <iframe
-                            src={place.map}
-                            width="100%"
-                            height="100%"
-                            className="border-0 rounded-xl"
-                            referrerPolicy="no-referrer-when-downgrade"
-                            title="Google Maps"
-                        ></iframe>
+                        {place.tag === "coxs-bazar" ? (
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59420.98450172475!2d91.95166092981037!3d21.436651710814438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30adc7ea2ab928c3%3A0x3b539e0a68970810!2sCox&#39;s%20Bazar!5e0!3m2!1sen!2sbd!4v1715006573381!5m2!1sen!2sbd"
+                                width="100%"
+                                height="100%"
+                                className="border-0 rounded-xl"
+                                allowFullScreen=""
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"
+                                title="Google Maps"
+                            ></iframe>
+                        ) : (
+                            <iframe
+                                src={place.map}
+                                width="100%"
+                                height="100%"
+                                className="border-0 rounded-xl"
+                                referrerPolicy="no-referrer-when-downgrade"
+                                title="Google Maps"
+                            ></iframe>
+                        )}
                     </div>
                 </div>
             </div>
